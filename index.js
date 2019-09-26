2673a63d18b6767d6e4a616f2682050d9eb32c53
JeremyWBarr <36244640+JeremyWBarr@users.noreply.github.com>
2019-09-26 10:52:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,86 @@
-var app     = require('express')();
-var http    = require('http').createServer(app);
-var io      = require('socket.io')(http);
+var express     = require('express');
+var app         = express();
+var path        = require('path');
+var http        = require('http').createServer(app);
+var io          = require('socket.io')(http);
+var numusers   = 0;
+var userlist  = [];
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.redirect('index.html');
 });
 
 io.on('connection', function(socket){
-    var nickname = "anon";
+    var usernum     = ""
+    var joined      = false;
 
     console.log('A user connected.');
+    socket.emit('playerupdate', userlist);
 
     socket.on('disconnect', function(){
         console.log('A user disconnected.');
+        userlist.forEach(function(user, index){
+            if(user.usernum == usernum) {
+                numusers--;
+                userlist.splice(index, 1);
+
+                userlist.forEach(function(user, index){
+                    user.usernum = (index+1).toString();
+                });
+            }
+        });
+        playerupdate();
     });
 
-    socket.on('nickname', function(nn){
-        nickname = nn;
+    socket.on('join', function(nn, c){
+        if(joined == false && numusers <= 4) {
+            joined = true;
+            numusers++;
+
+            usernum     = numusers.toString();
+
+            userlist.push({
+                'usernum':  usernum,
+                'nickname': nn, 
+                'color':    c,
+                'playerdata': {
+                    'x': 0,
+                    'y': 0
+                }
+            });
+            playerupdate();
+        } else {
+            userlist.forEach(function(user){
+                if(user.usernum == usernum) {
+                    user.nickname   = nn;
+                    user.color      = c;
+                    playerupdate();
+                }
+            });
+        }
+
+        socket.emit('joined', nn, c);
     });
 
-    socket.on('chat message', function(message){
-        io.emit('chat message', nickname+": "+message);
-        socket.emit('chat message', 'you spoke!');
+    socket.on('update', function(data){
+        userlist.forEach(function(user){
+            if(user.usernum == usernum) {
+                user.playerdata.x += 5*data.right;
+                user.playerdata.x -= 5*data.left;
+                user.playerdata.y += 5*data.down;
+                user.playerdata.y -= 5*data.up;
+                socket.emit('update', userlist);
+            }
+        });
     });
 });
+
+function playerupdate() {
+    console.log(userlist);
+    io.emit('playerupdate', userlist);
+}
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
